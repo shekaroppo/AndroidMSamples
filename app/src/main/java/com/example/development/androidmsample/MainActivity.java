f5b8f5eb5d7a16f55b82a776867ac1ee5408b396
Shekar <shekar.shine@gmail.com>
2015-06-09 01:03:14 +0530

    Added navigation drawer

--- app/src/main/java/com/example/development/androidmsample/MainActivity.java
+++ app/src/main/java/com/example/development/androidmsample/MainActivity.java
@@ -2,10 +2,11 @@ package com.example.development.androidmsample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
